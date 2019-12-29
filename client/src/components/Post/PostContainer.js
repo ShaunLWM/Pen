98042863e75655b3d147fcfbe6f36eb80123173e
ShaunLWM <shaunlimweimin@gmail.com>
2019-12-30 01:28:48 +0800

    Fix unused posttitle component
    Remove ugly padding from title

--- client/src/components/Post/PostContainer.js
+++ client/src/components/Post/PostContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PostSubtitle from "./PostSubtitle";
-import { Link } from "react-router-dom";
+import PostTitle from "./PostTitle";
 
-export default function PostContainer({ id, title }) {
-    return (<header>
-        {<Link to={`/id/${id}`} style={{ textDecoration: 'none' }}><h2 style={{ marginBottom: "0.4375rem" }}>{title}</h2></Link>}
-        <PostSubtitle />
-    </header>
+export default function PostContainer(props) {
+    return (
+        <header>
+            <PostTitle {...props} />
+            <PostSubtitle />
+        </header>
     )
 }
--- client/src/components/Post/PostTitle.js
+++ client/src/components/Post/PostTitle.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function PostTitle({ id, title, shouldLink = true }) {
     if (shouldLink) {
         return (
-            <Link to={`/id/${id}`} style={{ textDecoration: 'none' }}> <h2 style={{ marginBottom: "0.4375rem" }}>{title}</h2></Link>
+            <Link to={`/id/${id}`} style={{ textDecoration: 'none' }}> <h2 style={{ marginBottom: "0px" }}>{title}</h2></Link>
         )
     }
 
     return (
-        <h2 style={{ marginBottom: "0.4375rem" }}>{title}</h2>
+        <h2 style={{ marginBottom: "0px" }}>{title}</h2>
     )
 }
