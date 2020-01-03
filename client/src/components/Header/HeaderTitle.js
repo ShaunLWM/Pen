2ecb2f4f0a2031c0e8ede339d55cfc427b134c40
ShaunLWM <shaunlimweimin@gmail.com>
2020-01-03 21:09:07 +0800

    Force Link from material-ui for theming correction

--- client/src/components/Header/HeaderTitle.js
+++ client/src/components/Header/HeaderTitle.js
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, forwardRef } from "react";
 import { store } from "../../store";
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+
+const LinkBehavior = forwardRef((props, ref) => (
+    <RouterLink ref={ref} {...props} />
+));
 
 export default function HeaderTitle() {
     const globalState = useContext(store);
     return (
-        <Link to={`/`} style={{ textDecoration: 'none' }}>
+        <Link to={`/`} component={RouterLink} style={{ textDecoration: 'none' }}>
             <h1 className="header">{globalState["state"]["profile"]["name"]}</h1>
         </Link >
     );
--- client/src/components/Post/PostTitle.js
+++ client/src/components/Post/PostTitle.js
@@ -1,7 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from "prop-types";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { forwardRef } from "react";
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
     postTitle: {
@@ -10,10 +11,14 @@ const useStyles = makeStyles({
     },
 });
 
+const LinkBehavior = forwardRef((props, ref) => (
+    <RouterLink ref={ref} {...props} />
+));
+
 export default function PostTitle({ post_slug, post_title, shouldLink = true }) {
     const classes = useStyles();
     if (shouldLink)
-        return (<Link to={`/id/${post_slug}`} style={{ textDecoration: 'none' }}> <h3 className={classes.postTitle}>{post_title}</h3></Link>)
+        return (<Link component={RouterLink} to={`/id/${post_slug}`} style={{ textDecoration: 'none' }}> <h3 className={classes.postTitle}>{post_title}</h3></Link>)
     return (<h3 className={classes.postTitle}>{post_title}</h3>)
 }
 
